9668dd0675f27420caf60d62b3c8b5bca1c1ccd9
Shane O'Grady <shane.ogrady@gmail.com>
2013-03-15 18:51:26 +0000

    First go, with sample data

---
+++ js/geo.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+  var map = L.map('map');
+  L.tileLayer('http://{s}.tile.cloudmade.com/a777adab7b1d454f9290d5312d97ab23/{styleId}/256/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+      styleId: 997
+  }).addTo(map);
+
+  $.getJSON('sample.json', function(data) {
+
+    var bounds = new L.LatLngBounds();
+
+    $.each(data, function(key, obj) {
+
+      bounds.extend([obj.location.lat, obj.location.lng]);
+
+      var marker = L.marker([obj.location.lat, obj.location.lng], {
+        "riseOnHover": true
+      }).addTo(map);
+
+      marker.bindPopup("<b>" + obj.name + "</b><br>" + obj.description + "<br>" + moment(obj.timestamp).fromNow());
+
+    });
+
+    map.fitBounds(bounds.pad(0.5));
+  });
+});
