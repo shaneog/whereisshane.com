a15779678eb913631849ef8e165be3fc8a73acbf
Shane O'Grady <shane@ogrady.ie>
2022-04-07 18:29:26 -0700

    fix: mapbox API changes

--- js/geo.js
+++ js/geo.js
@@ -1,21 +1,25 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  "use strict";
+document.addEventListener('DOMContentLoaded', function (event) {
+  'use strict';
 
   var map = L.map('map');
   var current_place;
 
-
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    accessToken: 'pk.eyJ1Ijoic2hhbmVvZyIsImEiOiJ3M2pRekZzIn0.PBMF6qUP6jta6HsC-eU4PQ',
-    id: 'mapbox.streets',
-  }).addTo(map);
-
+  L.tileLayer(
+    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+    {
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1Ijoic2hhbmVvZyIsImEiOiJ3M2pRekZzIn0.PBMF6qUP6jta6HsC-eU4PQ',
+    }
+  ).addTo(map);
 
   var RedIcon = L.Icon.Default.extend({
     options: {
       imagePath: '/images/',
-      iconUrl: 'marker-icon-red.png'
-    }
+      iconUrl: 'marker-icon-red.png',
+    },
   });
   var redIcon = new RedIcon();
 
@@ -23,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var request = new XMLHttpRequest();
   request.open('GET', 'data/data.geojson', true);
 
-  request.onload = function() {
+  request.onload = function () {
     if (request.status >= 200 && request.status < 400) {
       var data = JSON.parse(request.responseText);
 
@@ -33,11 +37,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
         onEachFeature: function (feature, layer) {
           pu = layer.bindPopup(feature.properties.name);
 
-          if (feature.properties.current == true){
+          if (feature.properties.current == true) {
             pu.setIcon(redIcon);
             current_place = pu;
           }
-        }
+        },
       }).addTo(map);
 
       map.fitBounds(geojsonLayer.getBounds());
